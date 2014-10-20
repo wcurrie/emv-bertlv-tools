a35561e96eb5b89d23409b0f75d4ed2d3f35d175
Will Currie <will@currie.id.au>
2014-10-21 07:04:19 +1100

    Handle signature change in RootDecoder.

--- tvr-cmdline/src/main/java/io/github/binaryfoo/Main.java
+++ tvr-cmdline/src/main/java/io/github/binaryfoo/Main.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -41,8 +40,8 @@ public class Main {
     private static void printHelp() {
         System.out.println("Usage Main <decode-type> <value> [<tag-set>]");
         System.out.println("  <decode-type> is one of");
-        for (Map.Entry<String, TagInfo> tag : RootDecoder.getSupportedTags()) {
-            System.out.println("    " + tag.getKey() + ": " + tag.getValue().getLongName());
+        for (TagInfo tag: RootDecoder.getSupportedTags()){
+            System.out.println("    " + tag.getShortName() + ": " + tag.getLongName());
         }
         System.out.println("  <value> is the hex string or '-' for standard input");
         System.out.println("  <tag-set> is one of " + RootDecoder.getAllTagMeta() + " defaults to EMV");
