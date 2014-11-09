a7a7e1a6b4e5c64a2f2020f84b31c1a90936f4ec
Will Currie <will@currie.id.au>
2014-11-09 15:09:55 +1100

    Use toSimpleString on List<DecodedData>.

--- src/test/java/io/github/binaryfoo/controllers/DecodedAsStringMatcher.kt
+++ src/test/java/io/github/binaryfoo/controllers/DecodedAsStringMatcher.kt
@@ -3,11 +3,13 @@ package io.github.binaryfoo.controllers
 import io.github.binaryfoo.DecodedData
 import org.hamcrest.Description
 import org.hamcrest.TypeSafeMatcher
+import kotlin.platform.platformStatic
+import io.github.binaryfoo.toSimpleString
 
 public class DecodedAsStringMatcher(private val raw: String) : TypeSafeMatcher<List<DecodedData>>() {
 
     override fun matchesSafely(decodedData: List<DecodedData>): Boolean {
-        return raw == toString(decodedData, "")
+        return raw == toString(decodedData)
     }
 
     override fun describeTo(description: Description) {
@@ -15,26 +17,13 @@ public class DecodedAsStringMatcher(private val raw: String) : TypeSafeMatcher<L
     }
 
     override fun describeMismatchSafely(item: List<DecodedData>, mismatchDescription: Description) {
-        mismatchDescription.appendText(toString(item, ""))
+        mismatchDescription.appendText(toString(item))
     }
 
-    private fun toString(decoded: List<DecodedData>, indent: String): String {
-        val b = StringBuilder()
-        for (d in decoded) {
-            b.append(indent)
-            if ("" != d.rawData) {
-                b.append(d.rawData).append(": ")
-            }
-            b.append(d.getDecodedData()).append("\n")
-            b.append(toString(d.children, indent + "  "))
-        }
-        return b.toString()
-    }
+    private fun toString(decoded: List<DecodedData>): String = decoded.toSimpleString("")
 
     class object {
 
-        public fun decodedAsString(raw: String): TypeSafeMatcher<List<DecodedData>> {
-            return DecodedAsStringMatcher(raw)
-        }
+        platformStatic public fun decodedAsString(raw: String): TypeSafeMatcher<List<DecodedData>> = DecodedAsStringMatcher(raw)
     }
 }
